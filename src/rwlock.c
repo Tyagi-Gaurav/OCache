8a0630ed5f1642ed744091fb08022f918c89c6d1
Aneesah Quraishi and Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2020-08-17 16:14:26 +0100

    Read write lock implementation

--- src/rwlock.c
+++ src/rwlock.c
@@ -1,6 +1,7 @@
 #include "rwlock.h"
 #include <stdlib.h>
 #include <errno.h>
+#include <stdio.h>
 
 int init_lock(rwlock_t *lock) {
   int status;
@@ -35,8 +36,7 @@ int destroy_lock(rwlock_t *lock) {
     return EINVAL;
 
   status = pthread_mutex_lock(&lock->mutex);
-  if (status != 0)
-    return status;
+  if (status != 0) return status;
 
   //Mark the lock as invalid.
   lock->valid = 0;
@@ -48,10 +48,130 @@ int destroy_lock(rwlock_t *lock) {
   }
 
   while (lock->w_count > 0) {
-    //Wait for readers to finish.
+    //Wait for writers to finish.
     status = pthread_cond_wait(&lock->write_allowed, &lock->mutex);
     if (status != 0) return status;
   }
 
+  status = pthread_mutex_unlock(&lock->mutex);
+  if (status != 0) return status;
+
+  pthread_cond_destroy(&lock->read_allowed);
+  pthread_cond_destroy(&lock->write_allowed);
+  pthread_mutex_destroy(&lock->mutex);
   return 0;
+}
+
+int readlock(rwlock_t *lock) {
+  int status;
+  if (lock->valid != VALID_RW_LOCK)
+    return EINVAL;
+
+  status = pthread_mutex_lock(&lock->mutex);
+  if (status != 0) return status;
+
+  //Register interest for reading.
+  lock->r_count++;
+  
+  while (lock->w_count > 0) {
+    //There is exclusive write lock on the cache.
+    //Wait until write finishes its work.
+    status = pthread_cond_wait(&lock->read_allowed, &lock->mutex);
+    if (status != 0) {
+      pthread_mutex_unlock(&lock->mutex);
+      return status;
+    }
+  }
+  
+  status = pthread_mutex_unlock(&lock->mutex);
+  return status;
+}
+
+int writelock(rwlock_t *lock) {
+  int status;
+
+  if (lock->valid != VALID_RW_LOCK)
+    return EINVAL;
+
+  status = pthread_mutex_lock(&lock->mutex);
+  if (status != 0) return status;
+
+  /*
+    Check if previous writers have taken lock. 
+    If so, wait for them to finish.
+  */
+  while (lock->w_count > 0) {
+    status = pthread_cond_wait(&lock->write_allowed, &lock->mutex);
+    if (status != 0) {
+      pthread_mutex_unlock(&lock->mutex);
+      return status;
+    }
+  }
+  
+  lock->w_count++;
+
+  /*
+    Check if previous readers have taken lock. 
+    If so, wait for them to finish.
+  */
+  while (lock->r_count > 0) {
+    status = pthread_cond_wait(&lock->write_allowed, &lock->mutex);
+    if (status != 0) {
+      pthread_mutex_unlock(&lock->mutex);
+      return status;
+    }
+  }
+
+  return pthread_mutex_unlock(&lock->mutex);
+}
+
+int readunlock(rwlock_t *lock) {
+  int status, status2 = 0;
+
+  if (lock->valid != VALID_RW_LOCK)
+    return EINVAL;
+
+  status = pthread_mutex_lock(&lock->mutex);
+  if (status != 0) return status;
+
+  lock->r_count--;
+
+  if (lock->r_count > 0) {
+    status = pthread_mutex_unlock(&lock->mutex);
+    return status;
+  }
+
+  if (lock->w_count > 0) {
+    status2 = pthread_cond_signal(&lock->write_allowed);
+  }
+  
+  return status2 + pthread_mutex_unlock(&lock->mutex);
+}
+
+int writeunlock(rwlock_t *lock) {
+  int status, status2 = 0;
+
+  if (lock->valid != VALID_RW_LOCK) 
+    return EINVAL;
+
+  status = pthread_mutex_lock(&lock->mutex);
+  if (status != 0) return status;
+
+  lock->w_count--;
+
+  if (lock->r_count > 0) {
+    status2 = pthread_cond_broadcast(&lock->read_allowed);
+    if (status2 != 0) {
+      return pthread_mutex_unlock(&lock->mutex);
+    }
+  }
+
+  if (lock->w_count > 0) {
+    status2 = pthread_cond_signal(&lock->write_allowed);
+    if (status2 != 0) {
+      return pthread_mutex_unlock(&lock->mutex);
+    }
+  }
+
+  return pthread_mutex_unlock(&lock->mutex);
 }
--- test/rwlock_test.c
+++ test/rwlock_test.c
@@ -1,16 +1,17 @@
 #include <check.h>
 #include "../src/rwlock.h"
+#include <errno.h>
 
 START_TEST(test_init_rwlock) {
   rwlock_t lock;
 
   int status = init_lock(&lock);
   ck_assert_int_eq(status, 0);
-  rwlock_t *init_lock = &lock;
-  ck_assert_ptr_ne(init_lock, NULL);
-  ck_assert_int_eq(init_lock->r_count, 0);
-  ck_assert_int_eq(init_lock->w_count, 0);
-  ck_assert_uint_eq(init_lock->valid, VALID_RW_LOCK);
+  rwlock_t *lock_ptr = &lock;
+  ck_assert_ptr_ne(lock_ptr, NULL);
+  ck_assert_int_eq(lock_ptr->r_count, 0);
+  ck_assert_int_eq(lock_ptr->w_count, 0);
+  ck_assert_uint_eq(lock_ptr->valid, VALID_RW_LOCK);
 
   destroy_lock(&lock);
 }
@@ -19,12 +20,76 @@ START_TEST(test_destroy_lock) {
   rwlock_t lock;
 
   int status = init_lock(&lock);
-  ck_assert_int_eq(status, 0);
-  rwlock_t *init_lock = &lock;
+  rwlock_t *lock_ptr = &lock;
 
-  status = destroy_lock(&lock);
+  status = destroy_lock(lock_ptr);
   ck_assert_int_eq(status, 0);
-  ck_assert_uint_eq(init_lock->valid, 0);
+  ck_assert_uint_eq(lock_ptr->valid, 0);
+}
+
+START_TEST(test_read_lock_return_invalid_when_lock_not_initialized) {
+  rwlock_t lock;
+  
+  int status = readlock(&lock);
+  ck_assert_uint_eq(status, EINVAL);
+}
+
+START_TEST(test_read_lock_increments_readers) {
+  rwlock_t lock;
+
+  int status = init_lock(&lock);
+  ck_assert_int_eq(status, 0);
+  rwlock_t *lock_ptr = &lock;
+
+  status = readlock(lock_ptr);
+  ck_assert_int_eq(status, 0);
+
+  ck_assert_int_eq(lock_ptr->r_count, 1);
+}
+
+START_TEST(test_write_lock_increments_writers) {
+  rwlock_t lock;
+
+  int status = init_lock(&lock);
+  ck_assert_int_eq(status, 0);
+  rwlock_t *lock_ptr = &lock;
+
+  status = writelock(lock_ptr);
+  ck_assert_int_eq(status, 0);
+
+  ck_assert_int_eq(lock_ptr->w_count, 1);
+}
+
+START_TEST(test_write_unlock_decrements_writers) {
+  rwlock_t lock;
+
+  int status = init_lock(&lock);
+  ck_assert_int_eq(status, 0);
+  rwlock_t *lock_ptr = &lock;
+
+  status = writelock(lock_ptr);
+  ck_assert_int_eq(status, 0);
+  ck_assert_int_eq(lock_ptr->w_count, 1);
+
+  status = writeunlock(lock_ptr);
+  ck_assert_int_eq(status, 0);
+  ck_assert_int_eq(lock_ptr->w_count, 0);
+}
+
+START_TEST(test_read_unlock_decrements_readers) {
+  rwlock_t lock;
+
+  int status = init_lock(&lock);
+  ck_assert_int_eq(status, 0);
+  rwlock_t *lock_ptr = &lock;
+
+  status = readlock(lock_ptr);
+  ck_assert_int_eq(status, 0);
+  ck_assert_int_eq(lock_ptr->r_count, 1);
+
+  status = readunlock(lock_ptr);
+  ck_assert_int_eq(status, 0);
+  ck_assert_int_eq(lock_ptr->r_count, 0);
 }
 
 #ifndef __RWLOCK_SUITE_
@@ -39,6 +104,11 @@ Suite* rwlock_suite(void) {
 
   tcase_add_test(tc_core, test_init_rwlock);
   tcase_add_test(tc_core, test_destroy_lock);
+  tcase_add_test(tc_core, test_read_lock_return_invalid_when_lock_not_initialized);
+  tcase_add_test(tc_core, test_read_lock_increments_readers);
+  tcase_add_test(tc_core, test_write_lock_increments_writers);
+  tcase_add_test(tc_core, test_read_unlock_decrements_readers);
+  tcase_add_test(tc_core, test_write_unlock_decrements_writers);
 
   suite_add_tcase(s, tc_core);
   return s;
